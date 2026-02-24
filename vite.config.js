852d1288975405122b4705c4bb72cdd89e7ca7ad
injammahin <injammahin5507@gmail.com>
2026-02-24 22:37:33 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/app': 'http://localhost',
-    },
-  },
-  build: {
-    outDir: 'public/build',
-  },
-});
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    vue(),
+  ],
+})
