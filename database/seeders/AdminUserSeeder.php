e85dea2f509785698595f50298239c59a2bcdc8f
injammahin <injammahin5507@gmail.com>
2026-03-14 21:47:57 +0600

    udate

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -4,15 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
     public function run()
     {
         User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'), // Change the password
+            'name' => 'Mahin Admin',
+            'email' => 'mahin@gmail.com',
+            'password' => Hash::make('Mahin@5507'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('superadmin123'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
             'role' => 'admin',
         ]);
     }
